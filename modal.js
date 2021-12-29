602df2a932e4dccc6b0036a0dd2675c441cc98d9
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2021-12-29 14:59:07 +0100

    changes

--- modal.js
+++ modal.js
@@ -9,60 +9,18 @@ $(function () {
   var $formSignIn = $("form.sign-in");
   var $formRegister = $("form.register");
 
-//   var $firstChild = $("nav ul li:first-child");
-//   var $secondChild = $("nav ul li:nth-child(2)");
-//   var $thirdChild = $("nav ul li:nth-child(3)");
-
-  //defining function to create underline initial state on document load
-//   function initialState() {
-//     $(".underline").css({
-//       width: $firstChild.width(),
-//       left: $firstChild.position().left,
-//       top: $firstChild.position().top + $firstChild.outerHeight(true) + "px",
-//     });
-//   }
-//   initialState(); //() used after calling function to call function immediately on doc load
-
-  //defining function to change underline depending on which li is active
-//   function changeUnderline(el) {
-//     $(".underline").css({
-//       width: el.width(),
-//       left: el.position().left,
-//       top: el.position().top + el.outerHeight(true) + "px",
-//     });
-//   } //note: have not called the function...don't want it called immediately
-
-//   $firstChild.on("click", function () {
-//     var el = $firstChild;
-//     changeUnderline(el); //call the changeUnderline function with el as the perameter within the called function
-//     $secondChild.removeClass("active");
-//     $thirdChild.removeClass("active");
-//     $(this).addClass("active");
-//   });
-
-//   $secondChild.on("click", function () {
-//     var el = $secondChild;
-//     changeUnderline(el); //call the changeUnderline function with el as the perameter within the called function
-//     $firstChild.removeClass("active");
-//     $thirdChild.removeClass("active");
-//     $(this).addClass("active");
-//   });
-
-//   $thirdChild.on("click", function () {
-//     var el = $thirdChild;
-//     changeUnderline(el); //call the changeUnderline function with el as the perameter within the called function
-//     $firstChild.removeClass("active");
-//     $secondChild.removeClass("active");
-//     $(this).addClass("active");
-//   });
 
   $("#become_host").on("click", function () {
     $overlay.addClass("visible");
     $mainPopUp.addClass("visible");
-    $signIn.addClass("active");
-    $register.removeClass("active");
-    $formRegister.removeClass("move-left");
-    $formSignIn.removeClass("move-left");
+    $register.addClass("active");
+    $signIn.removeClass("active");
+    $formRegister.css({
+      display: "block",
+    });
+    $formSignIn.css({
+      display: "none",
+    });
   });
   $overlay.on("click", function () {
     $(this).removeClass("visible");
@@ -77,20 +35,28 @@ $(function () {
   $signIn.on("click", function () {
     $signIn.addClass("active");
     $register.removeClass("active");
-    $formSignIn.removeClass("move-left");
-    $formRegister.removeClass("move-left");
+    $formRegister.css({
+      display: "none",
+    });
+    $formSignIn.css({
+      display: "block",
+    });
     $mainPopUp.css({
-        height: 55 + "vh",
+      height:"400px"
     })
   });
 
   $register.on("click", function () {
     $signIn.removeClass("active");
     $register.addClass("active");
-    $formSignIn.addClass("move-left");
-    $formRegister.addClass("move-left");
+    $formRegister.css({
+      display: "block",
+    });
+    $formSignIn.css({
+      display: "none",
+    });
     $mainPopUp.css({
-        height: 75 + "vh",
+      height:"450px"
     })
   });
 
